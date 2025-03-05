7a3754d6f78278478bffc42bcfc43741bf507823
kiran s metri <kiransm787gmail.com>
2025-03-05 09:51:00 +0530

    Reset button update

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ btn.addEventListener('click',function() {
   //console.log("Color changed!")
 })
 
+let btn2 = document.querySelector('#reset');
+btn2.addEventListener('click', function(){
+  console.log("Button clicked");
+  const div = document.querySelector('div');
+  div.style.backgroundColor = '';
+  let h2 = document.querySelector('h2');
+  h2.innerText = 'Generate a random color';
+})
+
 function getRandomColor(){
   let red = Math.floor(Math.random()*255);
   let green = Math.floor(Math.random()*255);
